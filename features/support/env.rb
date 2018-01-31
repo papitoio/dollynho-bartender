d290c4e126ee9689a97299f06e146eb54d86f5b5
Papito <papito@MacBook-Pro-de-Papito.local>
2018-01-30 23:33:22 -0200

    add logs and ci params

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,9 +5,7 @@ require "capybara/cucumber"
 Capybara.configure do |c|
     c.default_driver = :selenium_chrome_headless
     c.app_host = 'https://dollynho-bartender.herokuapp.com'
-    # c.app_host = 'http://localhost:3000'
 end
 
-
-Capybara.default_max_wait_time = 5
+Capybara.default_max_wait_time = 10
 
