576e73d52d662541f9e2eb898b17f0f8934b2822
Fernando Papito <eu@papito.io>
2018-01-31 13:47:26 -0200

    add phantomjs

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,11 +1,15 @@
-require "selenium-webdriver"
-require "capybara"
-require "capybara/cucumber"
+require 'selenium-webdriver'
+require 'capybara'
+require 'capybara/cucumber'
+require 'capybara/poltergeist'
+
+Capybara.register_driver :selenium do |app|
+  Capybara::Poltergeist::Driver.new(app, js_errors: false)
+end
 
 Capybara.configure do |c|
-    c.default_driver = :selenium_chrome_headless
-    c.app_host = 'https://dollynho-bartender.herokuapp.com'
+  c.default_driver = :selenium
+  c.app_host = 'https://dollynho-bartender.herokuapp.com'
 end
 
 Capybara.default_max_wait_time = 10
-
