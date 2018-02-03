b5aac6d00e45b73ac15f9c64e9f3cc257b67e4c4
Papito <eu@papito.io>
2018-02-03 08:46:24 -0200

    chrome headless

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,12 +3,12 @@ require 'capybara'
 require 'capybara/cucumber'
 require 'capybara/poltergeist'
 
-Capybara.register_driver :selenium do |app|
-  Capybara::Poltergeist::Driver.new(app, js_errors: false)
-end
+# Capybara.register_driver :selenium do |app|
+#   Capybara::Poltergeist::Driver.new(app, js_errors: false)
+# end
 
 Capybara.configure do |c|
-  c.default_driver = :selenium
+  c.default_driver = :selenium_chrome_headless
   c.app_host = 'https://dollynho-bartender.herokuapp.com'
 end
 
